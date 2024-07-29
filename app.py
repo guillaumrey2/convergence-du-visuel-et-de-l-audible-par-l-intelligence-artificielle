22f6042a64d6479383c69b64fa7ebb2416243a91
guillaumrey2 <guillaume.rey@students.hevs.ch>
2024-07-29 17:20:45 +0200

    Update : app.py & loading.html update

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import os
 from werkzeug.utils import secure_filename
 import logging
 
+# Initialize Flask and logging
 logging.basicConfig(level=logging.DEBUG)
-
 app = Flask(__name__)
 
+# Configuration constants
 app.config['UPLOAD_FOLDER'] = '/home/guillaum.rey2@hevs.ch/convergence-du-visuel-et-de-l-audible-par-l-intelligence-artificielle/images'
 app.config['RECORDINGS_FOLDER'] = '/home/guillaum.rey2@hevs.ch/convergence-du-visuel-et-de-l-audible-par-l-intelligence-artificielle/recordings'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
+# Helper function to check file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
@@ -36,8 +38,8 @@ def loadImage():
 
 @app.route('/check_status/<filename>')
 def check_status(filename):
-    audio_file = os.path.join(app.config['RECORDINGS_FOLDER'], filename.rsplit('.', 1)[0] + '.wav')
-    if os.path.isfile(audio_file):
+    done_flag_path = os.path.join(app.config['RECORDINGS_FOLDER'], filename.rsplit('.', 1)[0] + '.wav.done')
+    if os.path.isfile(done_flag_path):
         return jsonify({'ready': True})
     return jsonify({'ready': False})
 
@@ -47,24 +49,22 @@ def recordings(filename):
     audio_filename = base_filename + '.wav'
     audio_file = os.path.join(app.config['RECORDINGS_FOLDER'], audio_filename)
     if os.path.isfile(audio_file):
-        # Pass the filename for downloading and the audio filename for playing
         return render_template('recordings.html', filename=filename, audio_filename=audio_filename)
     return "Recording not found", 404
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    base_path = os.path.join(app.config['UPLOAD_FOLDER'], filename.rsplit('.', 1)[0])
+    base_path = os.path.join(app.config['RECORDINGS_FOLDER'], filename.rsplit('.', 1)[0])
     audio_file_path = base_path + '.wav'
     done_flag_path = base_path + '.wav.done'
 
     if os.path.exists(done_flag_path):
         if os.path.exists(audio_file_path):
-            return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+            return send_from_directory(app.config['RECORDINGS_FOLDER'], os.path.basename(audio_file_path))
         else:
             return "Audio file not found, but flag file present.", 404
     else:
         return "Audio file is still processing.", 202
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
